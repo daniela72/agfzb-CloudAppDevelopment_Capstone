73fd86efe0409d1a1412c93a041561b397050fde
Daniela <36333589+daniela72@users.noreply.github.com>
2022-02-20 12:59:28 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -163,7 +163,8 @@ def add_review(request, dealer_id):
             json_payload = {}
             json_payload["review"] = review
             print(f"this is the payload {json_payload}")
-            json_result = post_request("https://2fe3d546.us-south.apigw.appdomain.cloud/api/review/", json_payload, dealer_id=dealer_id)
+            url = "https://2fe3d546.us-south.apigw.appdomain.cloud/api/review/"
+            json_result = post_request(url, json_payload, dealer_id=dealer_id)
             print("POST request result: ", json_result)
             try:
                 if json_result['ok']:
@@ -173,6 +174,6 @@ def add_review(request, dealer_id):
                     return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
             except:
                 print("post failed")
-                messages.add_message(request, messages.WARNING, json_payload)
+                messages.add_message(request, messages.WARNING, json_payload ' + json_result ' json_result)
             return render(request, 'djangoapp/add_review.html', context)
 
