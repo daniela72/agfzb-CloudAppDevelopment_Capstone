94ff25d6168582bbe01b87197f6af5d85684b441
Daniela <36333589+daniela72@users.noreply.github.com>
2022-02-21 11:46:03 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -177,5 +177,5 @@ def add_review(request, dealer_id):
                 messages.add_message(request, messages.WARNING, json_result)
                 return render(request, 'djangoapp/add_review.html', context)
             else:
-                return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+                return render(request, 'djangoapp:dealer_details', context)
 
